44fd3d7c2d9d39e1f4b29befea198a842d71b237
foxfluff <foxfluff@users.noreply.github.com>
2016-02-25 12:49:31 -0500

    Initial Commit of new structure

---
+++ ip.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+class Ip(object):
+
+    @property
+    def address(self):
+        pass
+
+
+    @address.setter
+    def address(self, address):
+        pass
+
+
+    def __init__(self, address):
+        pass
+
+
+    def __repr__(self):
+        pass
+
+
+    def __add__(self, other):
+        pass
+
+
+    def __sub__(self, other):
+        pass
+
+
+    def __and__(self, other):
+        pass
+
+
+    def __or__(self, other):
+        pass
+
+
+    def __xor__(self, other):
+        pass
+
+
+    def __eq__(self, other):
+        pass
+
+
+    def __ne__(self, other):
+        pass
+
+
+    def __gt__(self, other):
+        pass
+
+
+    def __ge__(self, other):
+        pass
+
+
+    def __lt__(self, other):
+        pass
+
+
+    def __le__(self, other):
+        pass
