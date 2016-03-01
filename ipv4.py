26f9e5dbf96a82655a28b8b7a27f280643d4b3f3
foxfluff <foxfluff@users.noreply.github.com>
2016-03-01 10:29:31 -0500

    Added support for IPv4 address assignment

--- ipv4.py
+++ ipv4.py
@@ -1,12 +1,33 @@
-# -*- coding: utf-8 -*-
-
 from .ip import Ip
 
 class Ipv4(Ip):
 
+    _delimiter = '.'
+
     @address.setter
     def address(self, address):
-        pass
+        def list_addr(addr):
+            if len(addr) != 4:
+                raise ValueError(
+                    'Expected 4 octet address, got %i in (%s)' %
+                        (len(addr), address)
+                    )
+            # converts a list into a single long
+            # [255, 255, 255, 255] -> 0b11111111 11111111 11111111 11111111
+            _addr = 0
+            for octet in range(4):
+                _addr += addr[octet] * 2 ** (8 * (3 - octet))
+            return _addr
+
+        def string_addr(addr):
+            return list_addr(
+                map(int, addr.split(self._delimiter))
+                )
+
+        if type(address) == str:
+            self._address = string_addr(address)
+        elif type(address) == tuple or type(address) == list:
+            self._address = list_addr(address)
 
 
     def __init__(self, address):
