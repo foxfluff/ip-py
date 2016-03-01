fff4886fe4e43fa609f23b4d52b21e8575d86c39
foxfluff <foxfluff@users.noreply.github.com>
2016-03-01 10:50:16 -0500

    Correcting syntax error (missing '=')

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from ipv6 import Ipv6
 __name__ = 'ip'
 __version__ = '0.1'
 __description__ = "Foxfluff's IPv4 and IPv6 datatype handling"
-__author__ 'foxfluff/luma'
+__author__ = 'foxfluff/luma'
 __url__ = 'https://github.com/foxfluff'
 
 __all__ = ['Ipv4', 'Ipv6']
