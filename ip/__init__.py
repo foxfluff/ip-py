0d571f3dbf743c6122ca5829900677724aa78713
foxfluff <foxfluff@users.noreply.github.com>
2016-03-03 13:32:34 -0500

    Increased version num, modified __all__ to represent to attributes

--- ip/__init__.py
+++ ip/__init__.py
@@ -1,16 +1,14 @@
-# -*- coding: utf-8 -*-
-
 from ip import Ip
 from ipv4 import Ipv4
 from ipv6 import Ipv6
 
 __name__ = 'ip'
-__version__ = '0.2'
+__version__ = '0.3'
 __description__ = "Foxfluff's IPv4 and IPv6 datatype handling"
 __author__ = 'foxfluff/luma'
 __url__ = 'https://github.com/foxfluff'
 
-__all__ = ['Ip', 'Ipv4', 'Ipv6']
+__all__ = ['Ip', 'Ipv4', 'Ipv6', 'IPV4_LOOPBACK', 'IPV4_GLOBAL_BROADCAST']
 
 IPV4_LOOPBACK = Ipv4([127, 0, 0, 1])
 IPV4_GLOBAL_BROADCAST = Ipv4([255, 255, 255, 255])
