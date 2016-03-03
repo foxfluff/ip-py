ce3fa9959b007ea76254f2b52f8e089a0c4c46d8
foxfluff <foxfluff@users.noreply.github.com>
2016-03-03 11:15:59 -0500

    Implemented address assignment and retrieval, making IPv6 funcitonal

--- ip/ipv6.py
+++ ip/ipv6.py
@@ -5,11 +5,11 @@ class Ipv6(Ip):
     _delimiter = ':'
     # technically speaking there is no global broadcast, need to find a better
     # term for this.
-    GLOBAL_BROADCAST = Ipv6("FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF")
+    GLOBAL_BROADCAST = "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"
 
     @property
     def address(self):
-        return super(Ipv6, self).address()
+        return super(Ipv6, self).address
 
     @address.setter
     def address(self, address):
@@ -23,10 +23,41 @@ class Ipv6(Ip):
                 #_addr += addr[octet] * 2 ** (8 * (3 - octet))
             #return _addr
             #eww copy paste
-            pass
+            _addr = 0
+            _digit = 0
+            for digit in range(8):
+                # assume hex if a string? base10 if an int? sure why not
+                # convert to int for ease of use I suppose.
+                if isinstance(addr[digit], str):
+                    _digit = int(addr[digit], 16)
+                elif (isinstance(addr[digit], int) or
+                    isinstance(addr[digit], long)):
+
+                    _digit = int(addr[digit])
+                _addr += _digit << (16 * (7 - digit))
+
+            return _addr
 
         def string_addr(addr):
-            pass
+            _addr = addr.split(self._delimiter)
+            if _addr[:2] == ['', ''] or _addr[-2:] == ['', '']:
+                # special case when '::' is on the outer most digit, causes
+                # .split to behave in a way we don't like
+                _addr.remove('')
+            if '' in _addr:
+                location = _addr.index('')
+                _addr.remove('')
+                _addr = (
+                    _addr[:location] +
+                    ([0] * (8 - len(_addr))) +
+                    _addr[location:])
+            if '' in _addr:
+                #indicates two '::', which is not possible in ipv6 addresses
+                print _addr, location
+                raise ValueError(
+                    'Expected at most one "%s", IPv6 does not support more' %
+                    (self._delimiter * 2))
+            return list_addr(_addr)
 
         if isinstance(address, str):
             self._address = string_addr(address)
@@ -47,12 +78,12 @@ class Ipv6(Ip):
     def _hex_digit(self, digit):
         # >>> hex(65535)[2:].rjust(4,'0')
         # 'ffff'
-        return hex(digit)[2:].rjust(4,'0')
+        return hex(digit)[2:].rjust(4, '0')
 
 
     def _dec_digit(self, digit):
         # this seems silly
-        return digit
+        return int(digit)
 
 
     def __init__(self, address):
@@ -60,7 +91,7 @@ class Ipv6(Ip):
 
 
     def __repr__(self):
-        return "%s('%s')" %("Ipv6", str(self))
+        return "%s('%s')" % ("Ipv6", str(self))
 
 
     def __str__(self):
@@ -72,7 +103,11 @@ class Ipv6(Ip):
 
 
     def __iter__(self):
-        pass
+        # GLORIOUS COPY PASTE/MINOR TWEAKS
+        addr = []
+        for digit in range(8):
+            addr += [65535 & self.address >> (16 * (7 - digit))]
+        return iter(map(self._dec_digit, addr))
 
 
     def __getitem__(self, index):
@@ -112,6 +147,6 @@ class Ipv6(Ip):
 
 
     def __invert__(self):
-        return Ipv6(self ^ self.GLOBAL_BROADCAST)
+        return Ipv6(self ^ Ipv6(self.GLOBAL_BROADCAST))
 
 __all__ = ['Ipv6']
